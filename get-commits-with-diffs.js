a887f8df5bb095ed344afc9c9d9a811ef49ed9cb
Christian Fei <crifei93@gmail.com>
2020-11-09 23:44:27 +0100

    show

--- get-commits-with-diffs.js
+++ get-commits-with-diffs.js
@@ -5,7 +5,7 @@ module.exports = async function getCommitsWithDiffs (length = 100) {
   const log = await git.log()
   const commits = log.all.filter(l => l.message.startsWith('Latest'))
   commits.length = length
-  const diffs = await Promise.all(commits.map(c => git.raw(`show ${c.hash}`, '--color=always')))
+  const diffs = await Promise.all(commits.map(c => git.show([`${c.hash}`, '--color=always'])))
   return commits.map((c, index) => Object.assign(c, {
     diff: diffs[index]
   }))
